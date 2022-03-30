0c275a756e18f71dc1199d2898d54f65f48e846e
Kevin Britz <kevin@equilibria.xyz>
2022-03-29 21:23:56 -0700

    add linear curve providers

---
+++ contracts/mocks/MockXLinearUtilizationCurveProvider.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "../curve/immutable/XLinearUtilizationCurveProvider.sol";
+
+contract MockXLinearUtilizationCurveProvider is XLinearUtilizationCurveProvider {
+    constructor(LinearUtilizationCurve memory initialUtilizationCurve)
+    XLinearUtilizationCurveProvider(initialUtilizationCurve)
+    { }
+
+    function computeUtilizationCurve(UFixed18 utilization) external view returns (Fixed18) {
+        return super._computeUtilizationCurve(utilization);
+    }
+}
