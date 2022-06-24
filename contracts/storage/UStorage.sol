cfd6898fd4425dfbbc8313d252669083ab572570
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2022-06-24 15:17:38 -0400

    [Partial Fix] I05 - Code, comments & documentation.

--- contracts/storage/UStorage.sol
+++ contracts/storage/UStorage.sol
@@ -3,24 +3,47 @@ pragma solidity ^0.8.13;
 
 import "../number/types/UFixed18.sol";
 
+/// @dev Stored boolean slot
 type BoolStorage is bytes32;
 using BoolStorageLib for BoolStorage global;
+
+/// @dev Stored Uint256 slot
 type Uint256Storage is bytes32;
 using Uint256StorageLib for Uint256Storage global;
+
+/// @dev Stored Int256 slot
 type Int256Storage is bytes32;
 using Int256StorageLib for Int256Storage global;
+
+/// @dev Stored Address slot
 type AddressStorage is bytes32;
 using AddressStorageLib for AddressStorage global;
+
+/// @dev Stored Bytes32 slot
 type Bytes32Storage is bytes32;
 using Bytes32StorageLib for Bytes32Storage global;
 
+/**
+ * @title BoolStorageLib
+ * @notice Library to manage storage and retrival of a boolean at a fixed storage slot
+ */
 library BoolStorageLib {
+    /**
+     * @notice Retrieves the stored value
+     * @param self Storage slot
+     * @return value Stored bool value
+     */
     function read(BoolStorage self) internal view returns (bool value) {
         assembly {
             value := sload(self)
         }
     }
 
+    /**
+     * @notice Stores the value at the specific slot
+     * @param self Storage slot
+     * @param value boolean value to store
+     */
     function store(BoolStorage self, bool value) internal {
         assembly {
             sstore(self, value)
@@ -28,13 +51,27 @@ library BoolStorageLib {
     }
 }
 
+/**
+ * @title Uint256StorageLib
+ * @notice Library to manage storage and retrival of an uint256 at a fixed storage slot
+ */
 library Uint256StorageLib {
+    /**
+     * @notice Retrieves the stored value
+     * @param self Storage slot
+     * @return value Stored uint256 value
+     */
     function read(Uint256Storage self) internal view returns (uint256 value) {
         assembly {
             value := sload(self)
         }
     }
 
+    /**
+     * @notice Stores the value at the specific slot
+     * @param self Storage slot
+     * @param value uint256 value to store
+     */
     function store(Uint256Storage self, uint256 value) internal {
         assembly {
             sstore(self, value)
@@ -42,13 +79,27 @@ library Uint256StorageLib {
     }
 }
 
+/**
+ * @title Int256StorageLib
+ * @notice Library to manage storage and retrival of an int256 at a fixed storage slot
+ */
 library Int256StorageLib {
+    /**
+     * @notice Retrieves the stored value
+     * @param self Storage slot
+     * @return value Stored int256 value
+     */
     function read(Int256Storage self) internal view returns (int256 value) {
         assembly {
             value := sload(self)
         }
     }
 
+    /**
+     * @notice Stores the value at the specific slot
+     * @param self Storage slot
+     * @param value int256 value to store
+     */
     function store(Int256Storage self, int256 value) internal {
         assembly {
             sstore(self, value)
@@ -56,13 +107,27 @@ library Int256StorageLib {
     }
 }
 
+/**
+ * @title AddressStorageLib
+ * @notice Library to manage storage and retrival of an address at a fixed storage slot
+ */
 library AddressStorageLib {
+    /**
+     * @notice Retrieves the stored value
+     * @param self Storage slot
+     * @return value Stored address value
+     */
     function read(AddressStorage self) internal view returns (address value) {
         assembly {
             value := sload(self)
         }
     }
 
+    /**
+     * @notice Stores the value at the specific slot
+     * @param self Storage slot
+     * @param value address value to store
+     */
     function store(AddressStorage self, address value) internal {
         assembly {
             sstore(self, value)
@@ -70,13 +135,27 @@ library AddressStorageLib {
     }
 }
 
+/**
+ * @title Bytes32StorageLib
+ * @notice Library to manage storage and retrival of a bytes32 at a fixed storage slot
+ */
 library Bytes32StorageLib {
+    /**
+     * @notice Retrieves the stored value
+     * @param self Storage slot
+     * @return value Stored bytes32 value
+     */
     function read(Bytes32Storage self) internal view returns (bytes32 value) {
         assembly {
             value := sload(self)
         }
     }
 
+    /**
+     * @notice Stores the value at the specific slot
+     * @param self Storage slot
+     * @param value bytes32 value to store
+     */
     function store(Bytes32Storage self, bytes32 value) internal {
         assembly {
             sstore(self, value)
