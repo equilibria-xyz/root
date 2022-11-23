71e7b17b5ef09a4951026ce0384ee41bb1bc27d6
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2022-11-23 13:48:15 -0500

    update comments

--- contracts/control/unstructured/CrossChainOwnable/UCrossChainOwnable_Arbitrum.sol
+++ contracts/control/unstructured/CrossChainOwnable/UCrossChainOwnable_Arbitrum.sol
@@ -6,10 +6,11 @@ import "./UCrossChainOwnable.sol";
 
 /**
  * @title UCrossChainOwnable_Arbitrum
- * @notice Library to manage the cross-chain ownership lifecycle of upgradeable contracts.
+ * @notice Library to manage the cross-chain ownership lifecycle of Arbitrum upgradeable contracts.
  * @dev This contract has been extended from the Open Zeppelin library to include an
  *      unstructured storage pattern so that it can be safely mixed in with upgradeable
- *      contracts without affecting their storage patterns through inheritance.
+ *      contracts without affecting their storage patterns through inheritance. This contract
+ *      is specific to the Arbitrum L2-side and should not be used on other chains.
  */
 abstract contract UCrossChainOwnable_Arbitrum is CrossChainEnabledArbitrumL2, UCrossChainOwnable {
     constructor() CrossChainEnabledArbitrumL2() {}
--- contracts/control/unstructured/CrossChainOwnable/UCrossChainOwnable_Optimism.sol
+++ contracts/control/unstructured/CrossChainOwnable/UCrossChainOwnable_Optimism.sol
@@ -9,9 +9,12 @@ import "./UCrossChainOwnable.sol";
  * @notice Library to manage the cross-chain ownership lifecycle of upgradeable contracts.
  * @dev This contract has been extended from the Open Zeppelin library to include an
  *      unstructured storage pattern so that it can be safely mixed in with upgradeable
- *      contracts without affecting their storage patterns through inheritance.
+ *      contracts without affecting their storage patterns through inheritance. This contract
+ *      is specific to the Optimism L2-side and should not be used on other chains.
  */
 abstract contract UCrossChainOwnable_Optimism is CrossChainEnabledOptimism, UCrossChainOwnable {
-    address constant L2_CROSS_DOMAIN_MESSENGER_ADDRESS = address(0x4200000000000000000000000000000000000007);
+    /// @dev System address for the CrossDomainMessenger. This address is ONLY valid on the L2 side
+    address public constant L2_CROSS_DOMAIN_MESSENGER_ADDRESS = address(0x4200000000000000000000000000000000000007);
+
     constructor() CrossChainEnabledOptimism(L2_CROSS_DOMAIN_MESSENGER_ADDRESS) {}
 }
