335a46e469d1e5d2b721a53ce5467bf5225643bf
Fennel <0xfennel@proton.me>
2023-02-16 03:28:49 +0900

    Preliminary Accumulator6

---
+++ contracts/accumulator/types/Accumulator6.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "../../number/types/Fixed6.sol";
+import "../../number/types/UFixed6.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
+/// @dev Accumulator6 type
+struct Accumulator6 {
+    Fixed6 _value;
+}
+
+using Accumulator6Lib for Accumulator6 global;
+struct StoredAccumulator6 {
+    int256 _value;
+}
+struct Accumulator6Storage { StoredAccumulator6 value; }
+using Accumulator6StorageLib for Accumulator6Storage global;
+
+
+/**
+ * @title Accumulator6Lib
+ * @notice Library that surfaces math operations for the signed Accumulator type.
+ * @dev TODO
+ */
+library Accumulator6Lib {
+    // TODO: Natspec
+    function accumulated(Accumulator6 memory self, Accumulator6 memory from, UFixed6 total) internal pure returns (Fixed6) {
+        return _mul(self._value.sub(from._value), total);
+    }
+
+    /**
+     * @notice Increments an accumulator by a given ratio
+     * @dev Always rounds down in order to prevent overstating the accumulated value
+     * @param self The accumulator to increment
+     * @param amount Numerator of the ratio
+     * @param total Denominator of the ratio
+     */
+    function increment(Accumulator6 memory self, Fixed6 amount, UFixed6 total) internal pure {
+        self._value = self._value.add(_div(amount, total));
+    }
+
+    /**
+     * @notice Decrements an accumulator by a given ratio
+     * @dev Always rounds down in order to prevent overstating the accumulated value
+     * @param self The accumulator to decrement
+     * @param amount Numerator of the ratio
+     * @param total Denominator of the ratio
+     */
+    function decrement(Accumulator6 memory self, Fixed6 amount, UFixed6 total) internal pure {
+        self._value = self._value.add(_div(amount.mul(Fixed6Lib.NEG_ONE), total));
+    }
+
+    function _div(Fixed6 amount, UFixed6 total) private pure returns (Fixed6) {
+        if (amount.sign() == -1) {
+            return Fixed6Lib.from(-1, amount.abs().divOut(total));
+        }
+        return amount.div(Fixed6Lib.from(total));
+    }
+
+    function _mul(Fixed6 amount, UFixed6 total) private pure returns (Fixed6) {
+        if (amount.sign() == -1) {
+            return Fixed6Lib.from(-1, amount.abs().mulOut(total));
+        }
+        return amount.mul(Fixed6Lib.from(total));
+    }
+}
+
+library Accumulator6StorageLib {
+    function read(Accumulator6Storage storage self) internal view returns (Accumulator6 memory) {
+        StoredAccumulator6 memory storedValue = self.value;
+        return Accumulator6(Fixed6.wrap(int256(storedValue._value)));
+    }
+
+    function store(Accumulator6Storage storage self, Accumulator6 memory newValue) internal {
+        self.value = StoredAccumulator6(Fixed6.unwrap(newValue._value));
+    }
+}
