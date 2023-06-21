d58e2b32ce48e9a82ca9e62d5cae5e40ce4337e5
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2023-06-21 12:18:51 -0400

    Unused import

--- contracts/token/types/Token6.sol
+++ contracts/token/types/Token6.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import "../../number/types/UFixed18.sol";
 import "../../number/types/UFixed6.sol";
 
 /// @dev Token6
