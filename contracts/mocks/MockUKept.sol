da9c52c75f58b60e2530c308619cec1a239bb956
arjun-io <2940142+arjun-io@users.noreply.github.com>
2023-07-21 12:58:21 -0400

    Add UKept (#68)
    * Add UPausable (#61)
    
    * Add UPausable
    
    Signed-off-by: Fennel <0xfennel@proton.me>
    
    * Address comments
    
    ---------
    
    Signed-off-by: Fennel <0xfennel@proton.me>
    
    * Add PController6 and PAccumulator6 (#62)
    
    * Add PController6
    
    * Add PAccumulator6
    
    * Remove .only
    
    * bug fixes and documentation
    
    * Fix and improve tests
    
    * Rename test file
    
    * add divide-by-2 note
    
    * Address comments, clean up tests
    
    ---------
    
    Co-authored-by: Kevin Britz <kevin@equilibria.xyz>
    
    * Add UKept
    
    * Fix tests for CI
    
    * Make tests work for both coverage and non-coverage tests
    
    * remove receiver, add natspec
    
    ---------
    
    Signed-off-by: Fennel <0xfennel@proton.me>
    Co-authored-by: 0xfennel <117121807+0xfennel@users.noreply.github.com>
    Co-authored-by: Kevin Britz <kevin@equilibria.xyz>
    Co-authored-by: Fennel <0xfennel@proton.me>

---
+++ contracts/mocks/MockUKept.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "../control/unstructured/UOwnable.sol";
+import "../keeper/UKept.sol";
+
+contract MockUKept is UKept, UOwnable {
+    /// @dev This event helps us test that `data` is being passed around correctly
+    event RaiseKeeperFeeCalled(UFixed18 amount, bytes data);
+
+    function initialize(AggregatorV3Interface ethTokenOracleFeed_, Token18 keeperToken_) external initializer(1) {
+        super.__UKept__initialize(ethTokenOracleFeed_, keeperToken_);
+        super.__UOwnable__initialize();
+    }
+
+    function _raiseKeeperFee(UFixed18 amount, bytes memory data) internal override {
+        emit RaiseKeeperFeeCalled(amount, data);
+        keeperToken().pull(owner(), amount);
+    }
+
+    function toBeKept(UFixed18 multiplier, uint256 buffer, bytes memory data) keep(multiplier, buffer, data) external {}
+
+    /// @dev This function is used to figure out what gasUsed is. We can't hardcode this
+    /// @dev in tests because it depends on whether we're running coverage or not.
+    function instrumentGas() external returns (uint256) {
+        uint256 startGas = gasleft();
+        emptyFunc();
+        return startGas - gasleft() - 24;
+    }
+
+    function emptyFunc() internal {}
+}
