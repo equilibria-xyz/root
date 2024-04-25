982d0ac00ad609e8d50a9f4c03cb667a890741b6
Kevin Britz <kevin@equilibria.xyz>
2024-04-24 21:48:29 -0700

    add noop adiabatic

--- contracts/mocks/MockInverseAdiabatic6.sol
+++ contracts/mocks/MockInverseAdiabatic6.sol
@@ -1,40 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-import "../adiabatic/types/InverseAdiabatic6.sol";
+import "../adiabatic/types/NoopAdiabatic6.sol";
 
-contract MockInverseAdiabatic6 {
-    function compute(
-        InverseAdiabatic6 memory self,
-        UFixed6 latest,
-        Fixed6 change,
-        UFixed6 price
-    ) external pure returns (Fixed6) {
-        return InverseAdiabatic6Lib.compute(self, latest, change, price);
+contract MockNoopAdiabatic6 {
+    function linear(NoopAdiabatic6 memory self, Fixed6 change, UFixed6 price) external pure returns (UFixed6) {
+        return NoopAdiabatic6Lib.linear(self, change, price);
     }
 
-    function exposure(InverseAdiabatic6 memory self, UFixed6 latest) external pure returns (Fixed6) {
-        return InverseAdiabatic6Lib.exposure(self, latest);
-    }
-
-    function linear(InverseAdiabatic6 memory self, Fixed6 change, UFixed6 price) external pure returns (UFixed6) {
-        return InverseAdiabatic6Lib.linear(self, change, price);
-    }
-
-    function proportional(InverseAdiabatic6 memory self, Fixed6 change, UFixed6 price) external pure returns (UFixed6) {
-        return InverseAdiabatic6Lib.proportional(self, change, price);
-    }
-
-    function adiabatic(InverseAdiabatic6 memory self, UFixed6 latest, Fixed6 change, UFixed6 price) external pure returns (Fixed6) {
-        return InverseAdiabatic6Lib.adiabatic(self, latest, change, price);
-    }
-
-    function update(
-        InverseAdiabatic6 memory self,
-        InverseAdiabatic6 memory newConfig,
-        UFixed6 latest,
-        UFixed6 price
-    ) external pure returns (Fixed6) {
-        return InverseAdiabatic6Lib.update(self, newConfig, latest, price);
+    function proportional(NoopAdiabatic6 memory self, Fixed6 change, UFixed6 price) external pure returns (UFixed6) {
+        return NoopAdiabatic6Lib.proportional(self, change, price);
     }
 }
