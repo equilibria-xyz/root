bf95d508dc01f05d7fded1f48580befda009e715
Ed Noepel <ed@noepel.net>
2024-06-26 15:30:26 -0400

    tidy

--- contracts/attribute/Factory.sol
+++ contracts/attribute/Factory.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-// TODO: should update all these to specific imports?
 import "@openzeppelin/contracts/utils/Create2.sol";
 import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 import "./interfaces/IFactory.sol";
--- contracts/mocks/MockFactory.sol
+++ contracts/mocks/MockFactory.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.13;
 
-// import "@openzeppelin/contracts/utils/Create2.sol";
 import "../attribute/Factory.sol";
 import "./MockInstance.sol";
 
@@ -22,13 +21,6 @@ contract MockFactory is Factory {
 
     function computeCreate2Address(string calldata name, bytes32 salt) external view returns (address) {
         return _computeCreate2Address(abi.encodeCall(MockInstance.initialize, (name)), salt);
-        // TODO: this works; move implementation to Factory
-        /*return address(uint160(uint256(keccak256(abi.encodePacked(
-            bytes1(0xff),
-            address(this),
-            salt,
-            keccak256(abi.encodePacked(type(BeaconProxy).creationCode, abi.encode(address(this), abi.encodeCall(MockInstance.initialize, (name)))))
-        )))));*/
     }
 
     function onlyCallableByInstance() external view onlyInstance {}
