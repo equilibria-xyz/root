7d90516bacb75f85f3437c405886d66a2d28f6f2
Kevin Britz <kevin@equilibria.xyz>
2024-11-13 23:30:24 -0800

    add tests

--- contracts/synbook/types/SynBook6.sol
+++ contracts/synbook/types/SynBook6.sol
@@ -6,10 +6,10 @@ import "../../number/types/UFixed6.sol";
 
 /// @dev SynBook6 type
 struct SynBook6 {
-    Fixed6 d0; // TODO: some of these should be unsigned, ensure p(s) > 0
-    Fixed6 d1;
-    Fixed6 d2;
-    Fixed6 d3;
+    UFixed6 d0;
+    UFixed6 d1;
+    UFixed6 d2;
+    UFixed6 d3;
     UFixed6 scale;
 }
 using SynBook6Lib for SynBook6 global;
@@ -32,48 +32,39 @@ library SynBook6Lib {
         Fixed6 change,
         UFixed6 price
     ) internal pure returns (Fixed6) {
-        (Fixed6 from, Fixed6 to, Fixed6 sign) = (
-            latest.div(Fixed6Lib.from(self.scale)),
-            latest.add(change).div(Fixed6Lib.from(self.scale)),
-            Fixed6Lib.from(change.sign(), UFixed6Lib.ONE)
-        );
+        // sign = 1 for buy / ask and -1 for sell / bid, use f(-x) for sell orders
+        if (change.lt(Fixed6Lib.ZERO)) {
+            latest = latest.mul(Fixed6Lib.NEG_ONE);
+            change = change.mul(Fixed6Lib.NEG_ONE);
+        }
 
-        Fixed6 spread = _indefinite(self.d0, self.d1, self.d2, self.d3, sign, to)
-            .sub(_indefinite(self.d0, self.d1, self.d2, self.d3, sign, from));
+        Fixed6 from = latest.div(Fixed6Lib.from(self.scale));
+        Fixed6 to = latest.add(change).div(Fixed6Lib.from(self.scale));
+        UFixed6 notional = change.abs().mul(price);
 
-        // TODO: put notional in indefinite for increased precision
-        return spread.mul(Fixed6Lib.from(change.abs())).mul(Fixed6Lib.from(price));
+        return _indefinite(self.d0, self.d1, self.d2, self.d3, to, notional)
+            .sub(_indefinite(self.d0, self.d1, self.d2, self.d3, from, notional));
     }
 
     /// @dev f(x) = d0 * x + d1 * x^2 / 2 + d2 * x^3 / 3 + d3 * x^4 / 4
-    /// @dev sign = 1 for buy / ask and -1 for sell / bid
     function _indefinite(
-        Fixed6 d0,
-        Fixed6 d1,
-        Fixed6 d2,
-        Fixed6 d3,
-        Fixed6 sign,
-        Fixed6 value
+        UFixed6 d0,
+        UFixed6 d1,
+        UFixed6 d2,
+        UFixed6 d3,
+        Fixed6 value,
+        UFixed6 notional
     ) private pure returns (Fixed6 result) {
-        Fixed6 x = value;
-        Fixed6 s = sign;
-
         // d0 * x
-        result = s.mul(x).mul(d0);
-        x = x.mul(value);
-        s = s.mul(sign);
+        result = Fixed6Lib.from(notional).mul(value).mul(Fixed6Lib.from(d0));
 
         // d1 * x^2 / 2
-        result = result.add(s.mul(x).mul(d1)).div(Fixed6Lib.from(2));
-        x = x.mul(value);
-        s = s.mul(sign);
+        result = result.add(Fixed6Lib.from(notional).mul(value).mul(value).mul(Fixed6Lib.from(d1)).div(Fixed6Lib.from(2)));
 
         // d2 * x^3 / 3
-        result = result.add(s.mul(x).mul(d2)).div(Fixed6Lib.from(3));
-        x = x.mul(value);
-        s = s.mul(sign);
+        result = result.add(Fixed6Lib.from(notional).mul(value).mul(value).mul(value).mul(Fixed6Lib.from(d2)).div(Fixed6Lib.from(3)));
 
         // d3 * x^4 / 4
-        result = result.add(s.mul(x).mul(d3)).div(Fixed6Lib.from(4));
+        result = result.add(Fixed6Lib.from(notional).mul(value).mul(value).mul(value).mul(value).mul(Fixed6Lib.from(d3)).div(Fixed6Lib.from(4)));
     }
 }
