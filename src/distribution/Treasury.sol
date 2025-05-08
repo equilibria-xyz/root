aae9425af08ac42ec81e8038ddb1ee177c77d28e
Prateek <prateek@perennial.finance>
2025-05-08 22:36:03 +0530

    Add credit, debit and reset methods to update allowance

--- src/distribution/Treasury.sol
+++ src/distribution/Treasury.sol
@@ -2,28 +2,40 @@
 pragma solidity ^0.8.13;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { Token, TokenLib } from "../token/types/Token.sol";
 import { Ownable } from "../attribute/Ownable.sol";
+import { ITreasury } from "./interfaces/ITreasury.sol";
 
-contract Treasury is Ownable {
-    using SafeERC20 for IERC20;
-
+contract Treasury is ITreasury, Ownable {
     function initialize() external initializer(1) {
         __Ownable__initialize();
     }
 
-    /// @notice Approves the transfer of ERC20 tokens from the escrow to a specified address.
-    /// @param token The address of the ERC20 token contract.
-    /// @param to The address to approve the tokens for.
-    /// @param amount The amount of tokens to approve.
-    function approve(address token, address to, uint256 amount) external onlyOwner {
-        IERC20(token).approve(to, amount);
+    /// @inheritdoc ITreasury
+    function approve(Token token, address to, uint256 amount) external onlyOwner {
+        TokenLib.approve(token, to, amount);
     }
 
-    /// @notice Deposits ERC20 tokens into the escrow contract.
-    /// @param token The address of the ERC20 token contract.
-    /// @param amount The amount of tokens to deposit.
-    function deposit(address token, uint256 amount) external {
-        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+    /// @inheritdoc ITreasury
+    function credit(Token token, address spender, uint256 creditAmount) external {
+        uint256 currentAllowance = IERC20(Token.unwrap(token)).allowance(address(this), spender);
+        token.approve(spender, currentAllowance + creditAmount);
+    }
+
+    /// @inheritdoc ITreasury
+    function debit(Token token, address spender, uint256 debitAmount) external {
+        uint256 currentAllowance = IERC20(Token.unwrap(token)).allowance(address(this), spender);
+        uint256 newAllowance = currentAllowance > debitAmount ? currentAllowance - debitAmount : 0;
+        token.approve(spender, newAllowance);
+    }
+
+    /// @inheritdoc ITreasury
+    function reset(Token token, address spender) external {
+        token.approve(spender, 0);
+    }
+
+    /// @inheritdoc ITreasury
+    function pull(Token token, address benefactor, uint256 amount) external {
+        TokenLib.pull(token, benefactor, amount);
     }
 }
---
+++ src/distribution/interfaces/ITreasury.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import { Token, TokenLib } from "../../token/types/Token.sol";
+
+interface ITreasury {
+    function initialize() external;
+
+    /// @notice Approves the transfer of ERC20 tokens from the escrow to a specified address.
+    /// @param token The address of the ERC20 token contract.
+    /// @param to The address to approve the tokens for.
+    /// @param amount The amount of tokens to approve.
+    function approve(Token token, address to, uint256 amount) external;
+
+    /// @notice Increases the approval for `spender` by `amount`
+    /// @param token Token to increase approval for
+    /// @param spender Address to increase approval for
+    /// @param creditAmount Amount to increase approval by
+    function credit(Token token, address spender, uint256 creditAmount) external;
+
+    /// @notice Decreases the approval for `spender` by `amount`
+    /// @param token Token to decrease approval for
+    /// @param spender Address to decrease approval for
+    /// @param debitAmount Amount to decrease approval by
+    function debit(Token token, address spender, uint256 debitAmount) external;
+
+    /// @notice Sets the approval for `spender` to zero
+    /// @param token Token to reset approval for
+    /// @param spender Address to reset approval for
+    function reset(Token token, address spender) external;
+
+    /// @notice Pulls ERC20 tokens from the benefactor to the treasury
+    /// @param token The address of the ERC20 token contract.
+    /// @param benefactor The address to pull the tokens from.
+    /// @param amount The amount of tokens to pull.
+    function pull(Token token, address benefactor, uint256 amount) external;
+}
